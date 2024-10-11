37fcd1bb7624ef3500b9ab910352694087860549
luvsuneja@gmail.com <luvsuneja@gmail.com>
2024-10-11 09:07:02 +0000

    Created Backend_Mailer and LICENSE.txt; Edited settings and Form1

---
+++ server_code/Backend_Mailer.py
@@ -0,0 +1,29 @@
+import anvil.email
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+def send_feedback(name,email, feedback):
+  anvil.email.send(to='noreply@anvil.works',
+                   subject="Feedback from {name}",
+                   text=f"""
+                   
+  A new person has filled out the feedback form!
+
+  Name: {name}
+  Email address: {email}
+  Feedback:
+  {feedback}
+  """)
+  
